15027afdb2206b750840f4d1775e61c42f5fb785
syroxceo <82676910+syroxceo@users.noreply.github.com>
2022-07-31 12:14:24 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+var first_name="Surapujita";
+
+var last_name="Seerla";
+
+var fullname= first_name.concat(last_name);
+
+console.log(fullname);
+
+var a = 10;
+
+var b = 5;
+
+var add = a + b;
+
+console.log(add);
